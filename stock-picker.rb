48fc1d16a7e4e234a0a76df6efe1165a236791db
murgraydj <murgraydj@gmail.com>
2022-11-11 00:54:43 -0500

    Adds empty stock-picker function

--- stock-picker.rb
+++ stock-picker.rb
@@ -1,3 +1,14 @@
 # Author: Murgray D. John
 # Date created: 11/10/2022 
 # Purpose: Takes in an array where each element represents a day's stock price, returns the best days to buy and sell.
+
+def stock_picker(stocks)
+    
+end
+
+def run_program
+    stocks = [17,3,6,9,15,8,6,1,10]
+    stock_picker(stocks)
+end
+
+run_program
